3911523f49216be80601a5bc25130c40b8249017
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2024-10-29 12:50:29 +0100

    added additional SH utils for light prior

--- utils/sh_utils.py
+++ utils/sh_utils.py
@@ -20,9 +20,40 @@
 #  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 #  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 #  POSSIBILITY OF SUCH DAMAGE.
+'''
+MIT License
 
+Copyright (c) 2018 Andrew Chalmers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+'''
+Utility functions to demonstrate usage of spherical harmonics
+'''
 import torch
 import numpy as np
+import cv2
+import utils.spherical_harmonics as  sh
+import utils.sh_additional_utils as utility
+
+
 # coefficients to evaluate Y_lm(phi,theta) using cartesian coordinates, with l>=0 and -l<=m<=l (l in N, m in Z)
 
 C0 = 0.28209479177387814   # Y_00
@@ -135,4 +166,50 @@ def gauss_weierstrass_kernel(roughness, sh_degree):
 
     return gw_kernel_sh
 
+
+def get_coefficients_matrix(xres,l_max=2):
+	yres = int(xres/2)
+	# setup fast vectorisation
+	x = np.arange(0,xres)
+	y = np.arange(0,yres).reshape(yres,1)
+
+	# Setup polar coordinates
+	lat_lon = xy_to_ll(x,y,xres,yres)
+
+	# Compute spherical harmonics. Apply thetaOffset due to EXR spherical coordiantes
+	Ylm = sh.sh_evaluate(lat_lon[0], lat_lon[1], l_max)
+	return Ylm
+
+
+def get_coefficients_from_image(ibl, l_max=2, resize_width=None, filder_amount=None):
+	# Resize if necessary (I recommend it for large images)
+	if resize_width is not None:
+		#ibl = cv2.resize(ibl, dsize=(resize_width,int(resize_width/2)), interpolation=cv2.INTER_CUBIC)
+		ibl = utility.resize_image(ibl, resize_width, int(resize_width/2), cv2.INTER_CUBIC)
+	elif ibl.shape[1] > 1000:
+		#print("Input resolution is large, reducing for efficiency")
+		#ibl = cv2.resize(ibl, dsize=(1000,500), interpolation=cv2.INTER_CUBIC)
+		ibl = utility.resize_image(ibl, 1000, 500, cv2.INTER_CUBIC)
+	xres = ibl.shape[1]
+	yres = ibl.shape[0]
+
+	# Pre-filtering, windowing
+	if filder_amount is not None:
+		ibl = blur_ibl(ibl, amount=filder_amount)
+
+	# Compute sh coefficients
+	sh_basis_matrix = get_coefficients_matrix(xres,l_max)
+
+	# Sampling weights
+	solid_angles = utility.get_solid_angle_map(xres)
+
+	# Project IBL into SH basis
+	n_coeffs = sh.sh_terms(l_max)
+	ibl_coeffs = np.zeros((n_coeffs,3))
+	for i in range(0,sh.sh_terms(l_max)):
+		ibl_coeffs[i,0] = np.sum(ibl[:,:,0]*sh_basis_matrix[:,:,i]*solid_angles)
+		ibl_coeffs[i,1] = np.sum(ibl[:,:,1]*sh_basis_matrix[:,:,i]*solid_angles)
+		ibl_coeffs[i,2] = np.sum(ibl[:,:,2]*sh_basis_matrix[:,:,i]*solid_angles)
+
+	return ibl_coeffs
 
