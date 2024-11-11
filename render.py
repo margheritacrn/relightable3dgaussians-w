175cb991fcc46fea390730618e7968f90056e1fc
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2024-11-11 14:08:19 +0100

    finalized rendering code

--- render.py
+++ render.py
@@ -31,15 +31,16 @@ import hydra
 #TODO: handle loading model at input iteration
 
 
-def render_lightings(model_path, name, iteration, model):
-    lighting_path = os.path.join(model_path, name, "iteration_{}".format(iteration))
+def render_lightings(model_path, iteration, model):
+    print(f"Rendering illuminations")
+    lighting_path = os.path.join(model_path, "rendered_envlights_sh/"+"iteration_{}".format(iteration))
     makedirs(lighting_path, exist_ok=True)
-    model.render_envlights_sh_all(path = lighting_path)
+    model.render_envlights_sh_all(save_path=lighting_path)
 
 
 def render_set(model_path, name, iteration, views, model, pipeline, background):
-    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+    render_path = os.path.join(model_path, name, "iteration_{}".format(iteration), "renders")
+    gts_path = os.path.join(model_path, name, "iteration_{}".format(iteration), "gt")
 
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
@@ -49,16 +50,20 @@ def render_set(model_path, name, iteration, views, model, pipeline, background):
 
         view_id = torch.tensor([view.uid], device = 'cuda')
         gt = view.original_image.cuda()
-        embedding_gt = model.embeddings(view_id)
+        if name == 'test':
+            embedding_gt = model.embeddings_test(view_id)
+        else:
+            embedding_gt = model.embeddings(view_id)
         envlight_sh = model.envlight_sh_mlp(embedding_gt)
         model.envlight.set_base(envlight_sh)
         render_pkg = render(view, model.gaussians, model.envlight, pipeline, background, debug=True)
+        render_pkg["render"] = torch.clamp(render_pkg["render"], 0.0, 1.0)
 
         torch.cuda.synchronize()
 
         gt = gt[0:3, :, :]
-        torchvision.utils.save_image(render_pkg["render"], os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+        torchvision.utils.save_image(render_pkg["render"], os.path.join(render_path, view.image_name + ".png"))
+        torchvision.utils.save_image(gt, os.path.join(gts_path, view.image_name + ".png"))
         for k in render_pkg.keys():
             if render_pkg[k].dim()<3 or k=="render" or k=="delta_normal_norm":
                 continue
@@ -72,28 +77,42 @@ def render_set(model_path, name, iteration, views, model, pipeline, background):
                 render_pkg[k] = 0.5 + (0.5*render_pkg[k])
             torchvision.utils.save_image(render_pkg[k], os.path.join(save_path, '{0:05d}'.format(idx) + ".png"))
 
-def render_sets(cfg, skip_train : bool, skip_test : bool, iteration: int):
+def render_sets(cfg, skip_train : bool, skip_test : bool):
     with torch.no_grad():
-        model = Relightable3DGW(cfg, load_iteration = iteration)
-        #gaussians = GaussianModel(cfg.dataset.sh_degree)
-        #scene = Scene(cfg.dataset, gaussians, load_iteration=iteration, shuffle=False)
+        model = Relightable3DGW(cfg)
 
         bg_color = [1,1,1] if cfg.dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
         if not skip_train:
-             render_set(cfg.dataset.model_path, "train", model.scene.loaded_iter, model.scene.getTrainCameras(), model, cfg.pipe, background)
+             render_set(cfg.dataset.model_path, "train", model.load_iteration, model.scene.getTrainCameras(), model, cfg.pipe, background)
 
         if not skip_test:
-             render_set(cfg.dataset.model_path, "test", model.scene.loaded_iter, model.scene.getTestCameras(), model, cfg.pipe, background)
+             model.optimize_embeddings_test()
+             render_set(cfg.dataset.model_path, "test", model.load_iteration, model.scene.getTestCameras(), model, cfg.pipe, background)
 
-        render_lightings(cfg.dataset.model_path, "rendered_envlights_sh", model)
+        render_lightings(model.config.dataset.model_path, model.load_iteration, model)
+
+def render_sets_training(model: Relightable3DGW, skip_train : bool, skip_test : bool):
+    with torch.no_grad():
+        bg_color = [1,1,1] if model.config.dataset.white_background else [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+
+        if not skip_train:
+             render_set(model.config.dataset.model_path, "train", model.load_iteration, model.scene.getTrainCameras(), model, model.config.pipe, background)
+
+        if not skip_test:
+             # Optimize embeddings for test set
+             model.optimize_embeddings_test()
+             render_set(model.config.dataset.model_path, "test", model.load_iteration, model.scene.getTestCameras(), model, model.config.pipe, background)
+
+        render_lightings(model.config.dataset.model_path, "rendered_envlights_sh", model) # only for train sofar
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="relightable3DG-W")
 def main(cfg: DictConfig):
-    print("Rendering " + cfg.cfg.dataset.model_path)
-    render_sets(cfg, cfg.skip_train, cfg.skip_test, cfg.iteration)
+    print("Rendering " + cfg.dataset.model_path)
+    render_sets(cfg, cfg.skip_train, cfg.skip_test)
     # All done
     print("\nRendering complete.")
 
@@ -105,12 +124,16 @@ if __name__ == "__main__":
     parser.add_argument("--skip_train", action="store_true")
     parser.add_argument("--skip_test", action="store_true")
     parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--source_path", type=str)
+    parser.add_argument("--model_path", type=str)
     args = parser.parse_args(sys.argv[1:])
 
     cl_args = [
-        f"+iteration={str(args.iteration)}",
+        f"dataset.model_path={args.model_path}",
+        f"dataset.source_path={args.source_path}",
+        f"load_iteration={str(args.iteration)}",
         f"+skip_train={args.skip_train}",
-        f"+skip_train={args.skip_test}"
+        f"+skip_test={args.skip_test}"
     ]
 
     # Initialize system state (RNG)
