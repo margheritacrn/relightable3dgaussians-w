d29fe8a3bab51810b63f5faffaa83f6140dbf3f7
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2024-11-20 17:18:24 +0100

    fixed bug

--- full_eval.py
+++ full_eval.py
@@ -17,9 +17,8 @@ parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
 parser.add_argument("--skip_rendering", action="store_true")
 parser.add_argument("--output_path", default="./eval")
+parser.add_argument("--num_sky_points", type=int, default=0)
 args, _ = parser.parse_known_args()
-
-nerfosr_scenes = []
 
 
 if not args.skip_training or not args.skip_rendering:
@@ -30,14 +29,14 @@ if not args.skip_training:
     common_args = " --quiet --eval --save_iterations 30_000 50_000 "
     for scene in nerfosr_scenes:
         source = args.nerfosr + "/" + scene
-        os.system("python train.py --source_path " + source + " --model_path " + args.output_path + "/" + scene + common_args)
+        os.system("python train.py --source_path " + source + " --model_path " + args.output_path + "/" + scene + " --num_sky_points " + str(args.num_sky_points) + common_args)
 
 if not args.skip_rendering:
     all_sources = []
     for scene in nerfosr_scenes:
         all_sources.append(args.nerfosr + "/" + scene)
 
-    common_args = " --quiet --eval "
+    common_args = " --quiet "
     for scene, source in zip(nerfosr_scenes, all_sources):
         os.system("python render.py --iteration 30000 --source_path " + source + " --model_path " + args.output_path + "/" + scene + common_args)
         os.system("python render.py --iteration 50000 --source_path " + source + " --model_path " + args.output_path + "/" + scene + common_args)
