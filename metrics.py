3d50ebd74655a94a1c19f8404c00ed963906e39a
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2024-12-05 11:41:00 +0100

    added metrics computation on full images

--- metrics.py
+++ metrics.py
@@ -21,9 +21,12 @@ from tqdm import tqdm
 from utils.image_utils import psnr
 from argparse import ArgumentParser
 import numpy as np
+from functools import reduce
+import torchvision
+from os import makedirs
 
 
-def readImages(renders_dir, gt_dir, masks_path=None, sky_masks_path=None):
+def readImages(renders_dir, gt_dir, masks_path=None, sky_masks_path=None, save_masked_imgs= False):
     renders = []
     gts = []
     image_names = []
@@ -33,10 +36,12 @@ def readImages(renders_dir, gt_dir, masks_path=None, sky_masks_path=None):
         render = tf.to_tensor(render).unsqueeze(0)[:, :3, :, :].cuda()
         gt = Image.open(gt_dir / fname)
         gt = tf.to_tensor(gt).unsqueeze(0)[:, :3, :, :].cuda()
+        masks =  []
         if sky_masks_path is not None:
             sky_mask_path = os.path.join(sky_masks_path, fname[:-4] + "_mask.png")
             sky_mask = Image.open(sky_mask_path).convert("L")
             sky_mask = tf.to_tensor(sky_mask).cuda()
+            masks.append(sky_mask)
         if masks_path is not None:
             mask_path = os.path.join(masks_path, fname)
             mask = Image.open(mask_path).convert("L")
@@ -51,8 +56,18 @@ def readImages(renders_dir, gt_dir, masks_path=None, sky_masks_path=None):
                 discarded_samples += 1
                 continue
             mask = mask.expand_as(gt)
-            renders.append((gt*mask)*sky_mask)
-            gts.append((render*mask)*sky_mask)
+            masks.append(mask)
+        if len(masks) > 0:
+            mask_final =  reduce(lambda x, y: x * y, masks)
+            gts.append(gt*mask_final)
+            renders.append(render*mask_final)
+            if save_masked_imgs:
+                rend_mask_path = os.path.join(renders_dir, "masked")
+                gt_mask_path = os.path.join(gt_dir, "masked")
+                makedirs(rend_mask_path, exist_ok=True)
+                makedirs(gt_mask_path, exist_ok=True)
+                torchvision.utils.save_image(render*mask_final, os.path.join(rend_mask_path, fname + ".png"))
+                torchvision.utils.save_image(gt*mask_final, os.path.join(gt_mask_path, fname + ".png"))
         else:
             renders.append(gt)
             gts.append(render)
@@ -61,7 +76,7 @@ def readImages(renders_dir, gt_dir, masks_path=None, sky_masks_path=None):
     return renders, gts, image_names
 
 
-def evaluate_half(model_paths, masks_path=None, sky_masks_path=None):
+def evaluate_full(model_path, test_dir = None, masks_path=None, sky_masks_path=None):
 
     full_dict = {}
     per_view_dict = {}
@@ -69,7 +84,66 @@ def evaluate_half(model_paths, masks_path=None, sky_masks_path=None):
     per_view_dict_polytopeonly = {}
     print("")
 
-    for scene_dir in model_paths:
+    for scene_dir in model_path:
+        print("Scene:", scene_dir)
+        full_dict[scene_dir] = {}
+        per_view_dict[scene_dir] = {}
+        full_dict_polytopeonly[scene_dir] = {}
+        per_view_dict_polytopeonly[scene_dir] = {}
+        
+        if test_dir is None:
+            test_dir = Path(scene_dir) / "test"
+
+        for method in os.listdir(test_dir):
+            print("Method:", method)
+
+            full_dict[scene_dir][method] = {}
+            per_view_dict[scene_dir][method] = {}
+            full_dict_polytopeonly[scene_dir][method] = {}
+            per_view_dict_polytopeonly[scene_dir][method] = {}
+
+            method_dir = test_dir / method
+            gt_dir = method_dir/ "gt"
+            renders_dir = method_dir / "renders"
+            renders, gts, image_names = readImages(renders_dir, gt_dir, masks_path, sky_masks_path, save_masked_imgs=True)
+
+            ssims = []
+            psnrs = []
+            lpipss = []
+
+            for idx in tqdm(range(len(renders)), desc="Metric evaluation progress"):
+                    _,C,H,W = renders[idx].shape
+                    ssims.append(ssim(renders[idx], gts[idx]))
+                    psnrs.append(psnr(renders[idx], gts[idx]))
+                    lpipss.append(lpips(renders[idx], gts[idx], net_type="vgg"))
+                    
+            print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
+            print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
+            print("  LPIPS: {:>12.7f}".format(torch.tensor(lpipss).mean(), ".5"))
+            print("")
+
+            full_dict[scene_dir][method].update({"SSIM": torch.tensor(ssims).mean().item(),
+                                                    "PSNR": torch.tensor(psnrs).mean().item(),
+                                                    "LPIPS": torch.tensor(lpipss).mean().item()})
+            per_view_dict[scene_dir][method].update({"SSIM": {name: ssim for ssim, name in zip(torch.tensor(ssims).tolist(), image_names)},
+                                                        "PSNR": {name: psnr for psnr, name in zip(torch.tensor(psnrs).tolist(), image_names)},
+                                                        "LPIPS": {name: lp for lp, name in zip(torch.tensor(lpipss).tolist(), image_names)}})
+
+            with open(scene_dir + "/results_full.json", 'w') as fp:
+                json.dump(full_dict[scene_dir], fp, indent=True)
+            with open(scene_dir + "/per_view_full.json", 'w') as fp:
+                json.dump(per_view_dict[scene_dir], fp, indent=True)
+
+
+def evaluate_half(model_path, masks_path=None, sky_masks_path=None):
+
+    full_dict = {}
+    per_view_dict = {}
+    full_dict_polytopeonly = {}
+    per_view_dict_polytopeonly = {}
+    print("")
+
+    for scene_dir in model_path:
         print("Scene:", scene_dir)
         full_dict[scene_dir] = {}
         per_view_dict[scene_dir] = {}
