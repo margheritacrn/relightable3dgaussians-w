87e586c421cbcbf4e3904888790c895a58b7a776
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2025-01-22 12:02:18 +0100

    added evaluation and rendering function for the scenes used for tuning hps

--- eval_with_gt_envmaps.py
+++ eval_with_gt_envmaps.py
@@ -15,6 +15,7 @@ from utils.loss_utils import mse2psnr, img2mae, img2mse, img2mse_image
 from utils.sh_additional_utils import get_coefficients_from_image
 import cv2
 import numpy as np
+import glob
 import torch
 import torchvision.transforms.functional as tf
 import os
@@ -47,7 +48,98 @@ def process_environment_map_image(img_path, scale_high, threshold):
     return coeffs
 
 
-def render_and_evaluate(cfg, eval_all=False):
+def render_and_evaluate_tuning_scenes(cfg, save_renders=False):
+    with torch.no_grad():
+        model = Relightable3DGW(cfg)
+
+        bg_color = [1,1,1] if cfg.dataset.white_background else [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+        test_cameras = model.scene.getTestCameras()
+        test_envmaps_path = os.path.join(cfg.dataset.source_path, "final/test/ENVMAP_CC") 
+        # Environment maps processing
+        scale = 10
+        threshold = 0.999
+        sun_angle_range = [0, 2*np.pi]
+        init_rot_x = -np.pi/2
+        init_rot_y = 0
+        init_rot_z = 0
+        psnrs = []
+
+        if save_renders:
+            renders_path = os.path.join(cfg.dataset.model_path, "eval_gt_envmap", "test", "iteration_{}".format(model.load_iteration), "renders")
+            gts_path = os.path.join(cfg.dataset.model_path, "eval_gt_envmap", "test", "iteration_{}".format(model.load_iteration), "gt")
+            makedirs(renders_path, exist_ok=True)
+            makedirs(gts_path, exist_ok=True)
+        
+        for view in tqdm(test_cameras):
+            if "_DSC" in view.image_name:
+                lighting_condition = view.image_name.split("_DSC")[0]
+            else:
+                lighting_condition = view.image_name.split("_IMG")[0]
+            
+            # Get processed envmap
+            envmap_path = os.path.join(test_envmaps_path, lighting_condition)
+            envmap_img_path = glob.glob(os.path.join(envmap_path, "*.jpg"))[0]
+            gt_envmap_sh = process_environment_map_image(envmap_img_path, scale, threshold) # (25,3)
+            # Get gt image
+            gt_image = view.original_image.cuda()
+            # Get mask
+            occluders_mask_path = os.path.join(cfg.dataset.source_path, "masks", view.image_name + ".png")
+            occluders_mask = cv2.imread(occluders_mask_path, cv2.IMREAD_GRAYSCALE)
+            occluders_mask = cv2.resize(occluders_mask, (gt_image.shape[2], gt_image.shape[1]))
+            sky_mask_path = os.path.join(cfg.dataset.source_path, "sky_masks", view.image_name + "_mask.png")
+            sky_mask = cv2.imread(sky_mask_path, cv2.IMREAD_GRAYSCALE)
+            sky_mask = cv2.resize(sky_mask, (gt_image.shape[2], gt_image.shape[1]))
+            mask = cv2.bitwise_and((sky_mask).astype(np.uint8), (occluders_mask).astype(np.uint8))
+            kernel = np.ones((5, 5), np.uint8)
+            mask = cv2.erode(mask, kernel, iterations=1)
+            mask = torch.from_numpy(mask//255).cuda()
+
+            best_psnr = 0
+            best_angle = None
+            all_psnrs = []
+
+            n = 101
+            sun_angles_prepare_list = torch.linspace(sun_angle_range[0], sun_angle_range[1], n)  
+            sun_angles = [torch.tensor([angle,0, 0]) for angle in sun_angles_prepare_list] #rotate only around y
+
+            for angle in tqdm(sun_angles):
+                # rotate envmap and render
+                gt_envmap_sh_rot = spaudiopy.sph.rotate_sh(gt_envmap_sh.T, init_rot_z, init_rot_y, init_rot_x, 'real')
+                gt_envmap_sh_rot = spaudiopy.sph.rotate_sh(gt_envmap_sh_rot, angle[2], angle[0], angle[1], 'real')
+                
+                gt_envmap_sh_rot = torch.tensor(gt_envmap_sh_rot.T, dtype=torch.float32, device="cuda")
+                model.envlight.set_base(gt_envmap_sh_rot)
+                render_pkg = render(view, model.gaussians, model.envlight, cfg.pipe, background, debug=False)
+                render_pkg["render"] = torch.clamp(render_pkg["render"], 0.0, 1.0)
+
+                # compute metrics
+                current_psnr = mse2psnr(img2mse(render_pkg["render"], gt_image, mask=mask))
+                all_psnrs.append(current_psnr.cpu())
+
+                if current_psnr > best_psnr:
+                    best_angle = angle
+                    best_psnr = current_psnr
+            
+            # Render with gtenvmap rotated according to best angles        
+            gt_envmap_sh_rot = spaudiopy.sph.rotate_sh(gt_envmap_sh.T, init_rot_z, init_rot_y, init_rot_x, 'real')
+            gt_envmap_sh_rot = spaudiopy.sph.rotate_sh(gt_envmap_sh_rot, best_angle[2], best_angle[0], best_angle[1], 'real')
+            gt_envmap_sh_rot = torch.tensor(gt_envmap_sh_rot, dtype=torch.float32, device="cuda")
+            model.envlight.set_base(gt_envmap_sh_rot.T)
+            render_pkg = render(view, model.gaussians, model.envlight, cfg.pipe, background, debug=False)
+            rendering = torch.clamp(render_pkg["render"]*mask, 0.0, 1.0)
+            psnrs.append(mse2psnr(img2mse(rendering, gt_image, mask=mask)))
+
+            if save_renders:
+                torch.cuda.synchronize()
+                gt_image = gt_image[0:3, :, :]
+                torchvision.utils.save_image(rendering, os.path.join(renders_path, view.image_name + ".png"))
+                torchvision.utils.save_image(gt_image*mask, os.path.join(gts_path, view.image_name + ".png"))
+            
+        return torch.tensor(psnrs).mean()
+
+
+def render_and_evaluate_test_scenes(cfg, eval_all=False):
     with torch.no_grad():
         model = Relightable3DGW(cfg)
 
@@ -171,11 +263,11 @@ def render_and_evaluate(cfg, eval_all=False):
             img_names.append(view.image_name)
             
             # Compute metrics
-            psnrs.append(mse2psnr(img2mse(render_pkg["render"], gt_image, mask=mask)))
-            maes.append(img2mae(render_pkg["render"], gt_image, mask=mask))
-            mses.append(img2mse(render_pkg["render"], gt_image, mask=mask))
+            psnrs.append(mse2psnr(img2mse(rendering, gt_image, mask=mask)))
+            maes.append(img2mae(rendering, gt_image, mask=mask))
+            mses.append(img2mse(rendering, gt_image, mask=mask))
 
-            rendered_np= render_pkg["render"].cpu().detach().numpy().transpose(1, 2, 0)
+            rendered_np= rendering.cpu().detach().numpy().transpose(1, 2, 0)
             gt_image_np = gt_image.cpu().detach().numpy().transpose(1, 2, 0)
             
             _, full = ssim_skimage(rendered_np, gt_image_np, win_size=5, channel_axis=2, full=True, data_range=1.0)
@@ -198,7 +290,7 @@ def render_and_evaluate(cfg, eval_all=False):
 def main(cfg: DictConfig):
     print("Rendering with GT illumination" + cfg.dataset.model_path)
     cfg.dataset.eval = True
-    render_and_evaluate(cfg, cfg.eval_all)
+    render_and_evaluate_test_scenes(cfg, cfg.eval_all)
     print("\nEnd")
 
 
