2ba969799efc930ae19f1b53eacd8c4f7e453d59
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2025-02-10 15:05:07 +0100

    added evaluation with gt envmaps

--- full_eval.py
+++ full_eval.py
@@ -11,7 +11,7 @@
 import os
 from argparse import ArgumentParser
 
-nerfosr_scenes = ["lk2", "schloss", "lwp", "st"]
+nerfosr_scenes = ["lk2", "lwp", "st"]
 
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument('--nerfosr', "-osr", required=True, type=str)
@@ -19,14 +19,17 @@ parser.add_argument("--skip_training", action="store_true")
 parser.add_argument("--skip_rendering", action="store_true")
 parser.add_argument("--skip_metrics", action="store_true")
 parser.add_argument("--output_path", default="./eval")
-parser.add_argument("--num_sky_points", type=int, default=0)
+parser.add_argument("--num_sky_points", type=int, default=50_000)
+parser.add_argument("--lambda_sky_gauss", type=float, default=0.05)
+parser.add_argument("--lambda_sky_spec_col", type=float, default=0.5)
 args, _ = parser.parse_known_args()
 
 if not args.skip_training:
-    common_args = " --quiet --eval --save_iterations 30_000 50_000 "
+    common_args = " --quiet --save_iterations 15_000 30_000 50_000 "
+    optimizer_args = f" --num_sky_points  {str(args.num_sky_points)} --lambda_sky_gauss {str(args.lambda_sky_gauss)} --lambda_sky_spec_col {str(args.lambda_sky_spec_col)} "
     for scene in nerfosr_scenes:
         source = args.nerfosr + "/" + scene
-        os.system("python train.py --source_path " + source + " --model_path " + args.output_path + "/" + scene + " --num_sky_points " + str(args.num_sky_points) + common_args)
+        os.system("python train.py --source_path " + source + " --model_path " + args.output_path + "/" + scene + optimizer_args + common_args)
 
 if not args.skip_rendering:
     all_sources = []
@@ -35,10 +38,10 @@ if not args.skip_rendering:
 
     common_args = " --quiet "
     for scene, source in zip(nerfosr_scenes, all_sources):
-        os.system("python render.py --iteration 30000 --source_path " + source + " --model_path " + args.output_path + "/" + scene + common_args)
+        # os.system("python render.py --iteration 30000 --source_path " + source + " --model_path " + args.output_path + "/" + scene + common_args)
         os.system("python render.py --iteration 50000 --source_path " + source + " --model_path " + args.output_path + "/" + scene + common_args)
 
-# reconstruction eval
+# Reconstruction eval
 if not args.skip_metrics:
     scenes_string = ""
     for scene in nerfosr_scenes:
@@ -48,4 +51,11 @@ if not args.skip_metrics:
         sky_masks_path = source + "/sky_masks" 
         os.system("python metrics.py -m " + scene_path + " --masks_path " + masks_path + " --sky_masks_path " + sky_masks_path)
 
-# eval with gt_envmaps
+# Eval with GT envmaps
+for scene in nerfosr_scenes:
+    scene_path = args.output_path + "/" + scene
+    source = args.nerfosr + "/" + scene
+    test_config_path = "./example_test_configs/" + scene
+    iterations = ["50_000"]
+    for iter in iterations:
+        os.system("python eval_with_gt_envmaps.py" + " --model_path " + scene_path + " --source_path " + source + " --test_config " + test_config_path + " --iteration " + iter)
