c8d4d975afaaba8b0094f244660581e04f325454
Margherita Lea Corona <margherita.lea.corona@hhi.fraunhofer.de>
2025-02-15 19:49:22 +0100

    updated 2DGS depth functions

--- utils/graphics_utils.py
+++ utils/graphics_utils.py
@@ -141,18 +141,18 @@ def normal_from_depth_image(depth, intrinsic_matrix, extrinsic_matrix):
 def depths_to_points(view, depthmap):
     c2w = (view.world_view_transform.T).inverse()
     W, H = view.image_width, view.image_height
-    ndc2pix = torch.tensor([
-        [W / 2, 0, 0, (W) / 2],
-        [0, H / 2, 0, (H) / 2],
-        [0, 0, 0, 1]]).float().cuda().T
-    projection_matrix = c2w.T @ view.full_proj_transform
-    intrins = (projection_matrix @ ndc2pix)[:3,:3].T
-    
+    fx = W / (2 * math.tan(view.FoVx / 2.))
+    fy = H / (2 * math.tan(view.FoVy / 2.))
+    intrins = torch.tensor(
+        [[fx, 0., W/2.],
+        [0., fy, H/2.],
+        [0., 0., 1.0]]
+    ).float().cuda()
     grid_x, grid_y = torch.meshgrid(torch.arange(W, device='cuda').float(), torch.arange(H, device='cuda').float(), indexing='xy')
     points = torch.stack([grid_x, grid_y, torch.ones_like(grid_x)], dim=-1).reshape(-1, 3)
     rays_d = points @ intrins.inverse().T @ c2w[:3,:3].T
     rays_o = c2w[:3,3]
-    points = (depthmap.reshape(-1,1))* rays_d + rays_o
+    points = depthmap.reshape(-1, 1) * rays_d + rays_o
     return points
 
 def depth_to_normal(view, depth):
